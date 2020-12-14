2686b602ffb128ff2c5adf80c23bdd8818f53ba4
JoyM <joy.js.mun@gmail.com>
2020-12-15 00:11:35 +0900

    ADD movie detail page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 import {Link} from "react-router-dom";
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id,year, title, summary, poster, genres }) {
   return (
+    <div className="movie">
     <Link to={{
-      pathname:'/movie-detail',
+      pathname:`/movie/${id}`,
       state:{
+        id,
         year, 
         title, 
         summary,
@@ -15,7 +17,6 @@ function Movie({ year, title, summary, poster, genres }) {
         poster
       }
     }}>
-      <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
@@ -29,8 +30,8 @@ function Movie({ year, title, summary, poster, genres }) {
         </ul>
         <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
-    </div>
     </Link>
+    </div>
   );
 }
 
