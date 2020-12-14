1907d47092752a1daec55ca270be6a5947e002f2
JoyM <joy.js.mun@gmail.com>
2020-12-15 00:16:39 +0900

    FIX detail.js page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Detail extends React.Component{
     componentDidMount(){
         const {location,history} = this.props;
-        if(location.state == undefined){
+        if(location.state === undefined){
             history.push('/');
         }
     }
