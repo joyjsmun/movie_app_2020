686dd6d4f30061d397443cc960ddaf03b4fd58c9
JoyM <joy.js.mun@gmail.com>
2020-12-14 23:42:07 +0900

    ADD react-router-dom

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import {Link} from "react-router-dom";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
+    <Link to={{
+      pathname:'/movie-detail',
+      state:{
+        year, 
+        title, 
+        summary,
+        genres,
+        poster
+      }
+    }}>
+      <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
@@ -19,6 +30,7 @@ function Movie({ year, title, summary, poster, genres }) {
         <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
+    </Link>
   );
 }
 
